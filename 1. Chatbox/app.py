f2b6eb25b74a754cc51b7fadeeb5c9d06aa73ff3
Ethan <147701278+Milk-Soda@users.noreply.github.com>
2023-12-12 11:30:12 +0100

    New version of Chatbot
    Update

---
+++ 1. Chatbox/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from Chatbot_camping import chat_bot
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_input = request.form["user_input"]
+    response = chat_bot(user_input)
+    return render_template("chat.html", user_input=user_input, response=response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
